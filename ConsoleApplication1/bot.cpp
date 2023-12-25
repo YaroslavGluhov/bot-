363fe23354a89f28db52de484c5076c9e063377f
YaroslavGluhov <glukhov.yar74@gmail.com>
2023-12-25 20:30:40 +0500

    first commit

---
+++ ConsoleApplication1/bot.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <tgbot/tgbot.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    TgBot::Bot bot("6777917201:AAFAJzQotfsEtFmZ3dMbSx40iORYSlpqbDk");
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hello!");
+        });
+    bot.getEvents().onCommand("help", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "I'm sure you can pass the exam without my help, good luck!");
+        });
+    bot.getEvents().onCommand("bye", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Bye, you will succeed!");
+        });
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+        printf("User wrote %s\n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start")) {
+            return;
+        }
+        if (StringTools::startsWith(message->text, "/help")) {
+            return;
+        }
+        if (StringTools::startsWith(message->text, "/bye")) {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+        });
+    try {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (true) {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    }
+    catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
+    return 0;
+}
